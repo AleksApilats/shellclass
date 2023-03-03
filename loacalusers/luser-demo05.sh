85adfacc32399cd0a221c9a2d055f84e1931dbfc
Aleksandrs Apilats <aleksandrs.apilats@hotmail.co.uk>
2023-03-03 16:02:34 +0000

    .

--- loacalusers/luser-demo05.sh
+++ loacalusers/luser-demo05.sh
@@ -4,3 +4,27 @@
 
 PASSWORD="${RANDOM}"
 echo "$PASSWORD"
+
+# Three random numbers together
+PASSWORD="${RANDOM}${RANDOM}${RANDOM}"
+echo "${PASSWORD}"
+
+# Set password to date +%s epoch time in force user to change it on first login in seconds
+PASSWORD=$(date +%s)
+echo ${PASSWORD}
+
+# Use nanoseconds to act as randomisation
+PASSWORD=$(date +%s%N)
+echo ${PASSWORD}
+
+# A better password randomisation
+PASSWORD=$(date +%s%N | sha256sum | head -c 32)
+echo ${PASSWORD}
+
+# Even stronger password randomisation 
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c 42)
+echo ${PASSWORD}
+
+#Append special character to the password
+SPECIAL_CHARACTER=$(echo '!@#$%^&*()_-+=' | fold -w1 | shuf | head -c1)
+echo "${PASSWORD}${SPECIAL_CHARACTER}"
---
+++ loacalusers/luser-demo06.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script generates a random password for each user specified on the command line
+
+# Display what the user typed on the command line
+
+echo "You executed this command: ${0}"
